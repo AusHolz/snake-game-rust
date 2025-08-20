1dd54b323ec8c27681e33978bc35a6f4fbb51791
David Konrad <dak.priv@proton.me>
2025-08-20 21:48:11 +0200

    First prototype implementation for snake game structs

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+use std::{collections::VecDeque, io::{self, Write}};
+use crossterm::{execute, terminal::{ScrollUp, SetSize, size}};
+use std::time::Duration;
+
+pub struct SnakeGame {
+    score: u32,
+    rows: u32,
+    columns: u32,
+    snake: VecDeque<Point2D>,
+    food_location: Point2D,
+    face_direction: Option<Direction>
+}
+
+impl SnakeGame {
+    pub fn move_snake(self: &Self, direction: Direction) -> Result<(), String> {
+        match direction {
+            Direction::Left => Ok(()),
+            Direction::Right => Ok(()),
+            Direction::Up => Ok(()),
+            Direction::Down => Ok(()),
+        }
+    }
+}
+
+#[derive(PartialEq)]
+pub enum Direction {
+    Left, Right, Up, Down
+}
+
+impl Direction {
+    pub fn are_opposite(self: &Self, other: &Direction) -> bool {
+        if self == &Direction::Left && other == &Direction::Right { true }
+        else if self == &Direction::Right && other == &Direction::Left { true }
+        else if self == &Direction::Up && other == &Direction::Down { true }
+        else if self == &Direction::Down && other == &Direction::Up { true }
+        else { false }
+    }
+}
+
+struct Point2D {
+    x: u32,
+    y: u32
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write};
+use crossterm::{execute, terminal::{ScrollUp, SetSize, size}};
+use std::time::Duration;
+
+fn main() -> io::Result<()> {
+    let (cols, rows) = size()?;
+    // Resize terminal and scroll up.
+    execute!(
+        io::stdout(),
+        SetSize(10, 10),
+        ScrollUp(5)
+    )?;
+
+    std::thread::sleep(Duration::new(2, 0));
+
+    // Be a good citizen, cleanup
+    execute!(io::stdout(), SetSize(cols, rows))?;
+    Ok(())
 }
